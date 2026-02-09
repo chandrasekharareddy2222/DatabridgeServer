a602b8bad082d0dcb7819d0886c592fb79938227
punjurideepak2001 <punjurideepak2001@gmail.com>
2026-02-09 17:35:35 +0530

    Update EmployeeController.cs

--- DatabridgeServer/Controllers/EmployeeController.cs
+++ DatabridgeServer/Controllers/EmployeeController.cs
@@ -12,7 +12,6 @@ namespace DatabridgeServer.Controllers
     public class EmployeeController : ControllerBase
     {
         private readonly IEmployeeService _employeeService;
-
         public EmployeeController(IEmployeeService employeeService)
         {
             _employeeService = employeeService;
@@ -24,51 +23,37 @@ namespace DatabridgeServer.Controllers
             var result = await _employeeService.GetAllEmployeesFullAsync();
             return Ok(result);
         }
-
+        
         [HttpPost("add")]
         public async Task<IActionResult> AddEmployee([FromBody] AddEmployeeRequest request)
         {
             if (!ModelState.IsValid) return BadRequest(ModelState);
-
             var result = await _employeeService.AddEmployeeAsync(request);
-
             return Ok(result);
         }
-
         [HttpGet("GetById/{id}")]
         public async Task<IActionResult> GetEmployeeById(int id)
         {
-
             var employee = await _employeeService.GetEmployeeByIdAsync(id);
-
             if (employee == null)
             {
                 return NotFound("Employee not found.");
             }
-
             return Ok(employee);
         }
-
         [HttpPut("update-name/{empId}")]
         public async Task<IActionResult> UpdateEmployeeName(
     int empId,
     [FromBody] UpdateEmployeeRequest request)
         {
             var message = await _employeeService.UpdateEmployeeNameAsync(empId, request.EmpName);
-
             return Ok(new { message });
         }
-
         [HttpDelete("{empId}")]
         public async Task<IActionResult> DeleteEmployee(int empId)
         {
             var result = await _employeeService.DeleteEmployeeAsync(empId);
             return Ok(result);
         }
-
-
-
-
-
     }
 }
