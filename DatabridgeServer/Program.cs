854d119af44254811b78369c28193d5b2446a89a
Devaraj Avunuri <avunuridevaraj@gmail.com>
2026-02-12 17:36:18 +0530

    merge conflicts resolved

--- DatabridgeServer/Program.cs
+++ DatabridgeServer/Program.cs
@@ -18,6 +18,8 @@ ExcelPackage.LicenseContext = LicenseContext.NonCommercial;
 // Add services to the container.
 builder.Services.AddControllers();
 
+
+
 // Configure Entity Framework Core with SQL Server
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
